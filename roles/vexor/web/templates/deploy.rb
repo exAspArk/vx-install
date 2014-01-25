6b77c13ed6eb339ac989be6e390493b7ff8815af
Dmitry Galinsky <dima.exe@gmail.com>
2014-01-25 20:40:11 +0400

    add postgres/install role

---
+++ roles/vexor/web/templates/deploy.rb
@@ -0,0 +1,27 @@
+require 'mina/bundler'
+require 'mina/git'
+
+set :version, "{{ vx_version }}"
+set :deploy_to, "{{ vx_home }}/worker"
+
+set :domain, 'worker.example.com'
+set :repository, 'git://github.com/vexor/vx-worker.git'
+set :branch, 'master'
+
+set :user, '{{ vx_user }}'
+
+task :environment do
+  queue %{
+    #{ echo_cmd %{export PATH={{ vx_home }}/bin:$PATH} }
+  }
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    invoke :'git:clone'
+    queue %{
+      bundle install --path=#{deploy_to}/shared/bundle --without test --jobs 4
+    }
+  end
+end
