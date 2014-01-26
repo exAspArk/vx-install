2dd6c496a229697c4e1b859d110ca3489111cfc6
Dmitry Galinsky <dima.exe@gmail.com>
2014-01-26 23:15:28 +0400

    add nginx and rename role defaults

--- roles/vexor/worker/templates/deploy.rb
+++ roles/vexor/worker/templates/deploy.rb
@@ -1,7 +1,7 @@
 require 'mina/bundler'
 require 'mina/git'
 
-set :version, "{{ vx_version }}"
+set :version, "{{ vx_worker_version }}"
 set :deploy_to, "{{ vx_home }}/worker"
 
 set :domain, 'worker.example.com'
